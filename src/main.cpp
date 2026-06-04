587e43bcbfa1ea81cda5b40800d4a90b76fc318d
kaualima7 <kaualima2007@outlook.com>
2026-06-04 12:40:19 -0300

    bibliotecas c++

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
 
 void exibirGrafo();
 void encontrarMenorCaminho();
@@ -8,28 +8,28 @@ void identificarRoteadoresCriticos();
 
 int main(int argc, char *argv[]){
 
-    if (argc != 2){ //verifica argumento da linha de comando
-        printf("Uso: %s <arquivo.log>\n", argv[0]);
+    if (argc != 2){ 
+        std::cout << "Uso: " << argv[0] << " <arquivo.log>\n";
         return 1;
     }
 
-    FILE *arquivo = fopen(argv[1], "r"); //tenta abrir o arquivo
+    std::ifstream arquivo(argv[1]);
 
-    if (arquivo == NULL){ //verifica se o arquivo foi aberto com sucesso
-        printf("Erro ao abrir o arquivo %s\n", argv[1]);
+    if (!arquivo.is_open()){
+        std::cout << "Erro ao abrir arquivo " << argv[1] << "\n";
         return 1;   
     }
 
-    printf("Carregando arquivo %s...\n", argv[1]);
+    std::cout << "Carregando arquivo " << argv[1] << "...\n";
 
     // TODO:
     // - Ler arquivo de entrada 
     // - Construir lista de adjacência
     // - Contar vértices e arestas
 
-    fclose(arquivo);
+    arquivo.close();
 
-    printf("Grafo de roteamento inicializado!\n");
+    std::cout << "Grafo de roteamento inicializado\n";
 
     return 0;
 }
